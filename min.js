5eac7f11070194326f114e0ab05d2da36205626f
hightech <office@webserveri.info>
2013-09-22 19:17:35 +0200

    Update min.js

--- min.js
+++ min.js
@@ -1,3 +1,10 @@
+//html5 ie hack
+document.createElement("article");
+document.createElement("footer");
+document.createElement("header");
+document.createElement("hgroup");
+document.createElement("nav");
+
 // select active tag
 function getSelect(s) {
   'use strict';
