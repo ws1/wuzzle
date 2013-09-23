1c9c413d5ff7c0f7434a25ff8ebef1dcecc778ca
Zlatan Vasović <legospace9876@gmail.com>
2013-09-23 16:48:34 +0200

    Core directory organization

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-wuzzle
-======
+# Wuzzle
 
-Our CSS puzzle.
+**Our UI puzzle.**
+
+Contains funny CSS and JS.
+
+Licensed under the terms of GPL v3 license.
---
+++ dist/js/wuzzle.js
@@ -0,0 +1,7 @@
+/*! Wuzzy | GPL v3 license | http://git.io/wuzzle */
+
+// Select active tag
+function getSelect(s) {
+  'use strict';
+  return s.options[s.selectedIndex].value;
+}
