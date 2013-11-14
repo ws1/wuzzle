67df22decb4ed95cab0f5b3f243fff66146ab6bf
Zlatan Vasović <legospace9876@gmail.com>
2013-11-14 21:42:25 +0100

    Remove JavaScript component(s)

--- src/js/wuzzle.js
+++
@@ -1,7 +0,0 @@
-/*! Wuzzle | GPL v3 license | http://git.io/wuzzle */
-
-// Simple function to select active tag
-function getSelect(s) {
-  'use strict';
-  return s.options[s.selectedIndex].value;
-}
